631a1ed830c6b549e3e024ef82c4ad72d1685e38
sufiatmi <48680860+sufiatmi@users.noreply.github.com>
2019-04-02 08:46:39 +0700

    Add files via upload

---
+++ no2a.java
@@ -0,0 +1,38 @@
+package jurnal;
+import java.util.Scanner;
+
+public class jurnal {
+
+ public static void main(String[] args){
+ int tinggi, setengahTinggi;
+ Scanner data = new Scanner(System.in);
+    System.out.print("Masukkan Angka : ");
+    
+    tinggi = data.nextInt();
+ int l = 1;
+ int n = tinggi/2;
+ System.out.println();
+ System.out.println("output: ");
+ for(int i=1;i<=n;i++){
+     for(int k=n;k>i;k--){
+         System.out.print(" ");
+     }
+     for(int j=1;j<=l;j++){
+         System.out.print("*");
+     }
+         l+=2;
+         System.out.println("");
+ }
+ l -= 4;
+ for(int i=(n-1);i>=1;i--){
+     for(int k=i;k<=(n-1);k++){
+         System.out.print(" ");
+     }
+     for(int j=l;j>=1;j--){
+         System.out.print("*");
+     }
+     l-=2;
+     System.out.println("");
+ }
+ } 
+}
